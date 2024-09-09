0e5a90ed1752c58d847db07e041ee5bc06326818
cactus1386 <cactusinjast@gmail.com>
2024-09-08 17:14:59 -0700

    add router

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Nav from '../src/components/Nav';
-import Home from './components/products/mostSeller';
+import Home from './components/Home';
 import Hoe from './components/products/products';
 import Footer from './components/footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (<>
-      <Nav />
-      <Home />
-
-      <Footer />
+    <Nav />
+    <Router>
+      <Routes>
+        <Route path='' element={<Home />} />
+      </Routes>
+    </Router>
+    <Footer />
   </>);
 }
 
