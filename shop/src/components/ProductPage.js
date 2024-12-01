1524e12a247820274c9305672a44ff5a62e1d730
ARS6666 <arsprogramming123@gmail.com>
2024-11-30 21:41:52 -0800

    PI fixed

--- shop/src/components/ProductPage.js
+++ shop/src/components/ProductPage.js
@@ -7,11 +7,6 @@ function ProductPage() {
   return (<>
     <PI />
     <CommentBox />
-    <div class="col-md-12 d-flex justify-content-center pt-5">
-      <div class="d-flex justify-content-end col-md-11 border-bottom">
-        <span class=" fontr h3  align-self-center">محصولات مشابه</span>
-      </div>
-    </div>
     <CommonProducts />
   </>);
 }
