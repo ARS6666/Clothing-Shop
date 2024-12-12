a9a75242f7c0e2f5d6ba1035e13a8e594906f616
ARS6666 <ARSprogramming123@gmail.com>
2024-12-12 11:27:42 -0800

    Offprdct.js text display changed

--- shop/src/components/Home.js
+++ shop/src/components/Home.js
@@ -10,11 +10,6 @@ function Home() {
     <Example />
     <Cat />
     <Prod />
-    <div class="col-md-12 d-flex justify-content-center">
-      <div class="d-flex justify-content-end col-md-11 border-bottom">
-        <span class=" fontr h3  align-self-center">تخفیف ها</span>
-      </div>
-    </div>
     <OFF />
   </>);
 }
--- shop/src/components/products/Offprdct.js
+++ shop/src/components/products/Offprdct.js
@@ -13,6 +13,7 @@ import { useLocation } from "react-router-dom";
 const ProductCarousel = () => {
   const sliderRef = useRef(null);
   const [products, setproducts] = useState([]);
+  const [OffDis, setOffDis] = useState(false);
 
   useEffect(() => {
     const myHeaders = new Headers();
@@ -29,6 +30,9 @@ const ProductCarousel = () => {
       .then((response) => response.json())
       .then((result) => setproducts(result))
       .catch((error) => console.error(error));
+    if (products.lenght == 5) {
+      setOffDis(true)
+    }
   }, []);
 
 
@@ -45,61 +49,68 @@ const ProductCarousel = () => {
 
   };
 
-  return (
-    <div className="carousel-container">
-      <Button
-        className="carousel-button border-0 left"
-        onClick={() => sliderRef.current.slickPrev()}
-      >
-        {"<"}
-      </Button>
-      <Slider ref={sliderRef} {...settings}>
-        {products?.map((c, index) => (
-          <div class="p-3 col-md-3 fontr" style={{ minWidth: `(-${(100 / 4)}%)` }}>
-            <div class={`${c.count === 0 ? 'out-of-stock col-md-12' : ' product-carde'}`}>
-              <div class="row m-0">
-                {c.discount != 0 ? <div class="discountDisplay"><span class="">{c.discount}%</span></div> : null}
-                <div class="d-flex justify-content-center ">
-                  <img
-                    src={c.pic}
-                    class="Img col-md-11 p-2"
-                  />
-                </div>
-                <div class="d-flex justify-content-center pt-2">
-                  <span class="h3 fontr text-center h5">{c.name}</span>
-                </div>
-                <div class="d-flex justify-content-center ">
-                  <span class="h5 fontr pt-1" dir="rtl">
-                    {c.price} هزار تومن
-                  </span>
-                </div>
-              </div>
-              <a class="hrefb align-self-center" href={"pi?id=" + c.id}>
-                <div className="hover-detailse col-md-12 ">
-                  <div
-                    class="d-flex justify-content-center "
-                    style={{ height: "400px" }}
-                  >
-                    <a class="hrefb align-self-center" href={"pi?id=" + c.id}>
-                      <button className="btn btn-light fontr ">
-                        مشاهده محصول
-                      </button>
-                    </a>
+  return (<>
+
+    {OffDis ? <></> :<><div class="col-md-12 d-flex justify-content-center">
+      <div class="d-flex justify-content-end col-md-11 border-bottom">
+        <span class=" fontr h3  align-self-center">تخفیف ها</span>
+      </div>
+    </div>
+      <div className="carousel-container">
+        <Button
+          className="carousel-button border-0 left"
+          onClick={() => sliderRef.current.slickPrev()}
+        >
+          {"<"}
+        </Button>
+        <Slider ref={sliderRef} {...settings}>
+          {products?.map((c, index) => (
+            <div class="p-3 col-md-3 fontr" style={{ minWidth: `(-${(100 / 4)}%)` }}>
+              <div class={`${c.count === 0 ? 'out-of-stock col-md-12' : ' product-carde'}`}>
+                <div class="row m-0">
+                  {c.discount != 0 ? <div class="discountDisplay"><span class="">{c.discount}%</span></div> : null}
+                  <div class="d-flex justify-content-center ">
+                    <img
+                      src={c.pic}
+                      class="Img col-md-11 p-2"
+                    />
+                  </div>
+                  <div class="d-flex justify-content-center pt-2">
+                    <span class="h3 fontr text-center h5">{c.name}</span>
+                  </div>
+                  <div class="d-flex justify-content-center ">
+                    <span class="h5 fontr pt-1" dir="rtl">
+                      {c.price} هزار تومن
+                    </span>
                   </div>
                 </div>
-              </a>
+                <a class="hrefb align-self-center" href={"pi?id=" + c.id}>
+                  <div className="hover-detailse col-md-12 ">
+                    <div
+                      class="d-flex justify-content-center "
+                      style={{ height: "400px" }}
+                    >
+                      <a class="hrefb align-self-center" href={"pi?id=" + c.id}>
+                        <button className="btn btn-light fontr ">
+                          مشاهده محصول
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
-      </Slider>
-      <Button
-        className="carousel-button border-0 right"
-        onClick={() => sliderRef.current.slickNext()}
-      >
-        {">"}
+          ))}
+        </Slider>
+        <Button
+          className="carousel-button border-0 right"
+          onClick={() => sliderRef.current.slickNext()}
+        >
+          {">"}
 
-      </Button>
-    </div>
+        </Button>
+      </div></>}
+  </>
   );
 };
 
